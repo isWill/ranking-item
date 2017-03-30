160d8aeab4e27833cb0dee8f7096a505f2fa0985
isWill <wilhelm2014@163.com>
2017-03-30 22:37:23 +0800

    通过路由改变页面内容

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div id="app">
     <split></split>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/api/appData?country=cn">cn</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=en">en</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=jp">jp</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=kr">kr</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=tw">tw</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=hk">hk</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/api/appData?country=fr">fr</router-link>
+      </div>
+    </div>
     <list></list>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
   import split from '@/components/split/split';
   import list from '@/components/list/list';
 
@@ -17,5 +41,18 @@
   };
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    border-bottom: 1px solid #eee
+    .tab-item
+      flex: 1
+      & > a
+        display: block
+        height: .8rem
+        line-height: .8rem
+        text-align: center
+        font-size: .3rem
+        &.router-link-active
+          color: #42b983
 </style>
--- src/components/split/split.vue
+++ src/components/split/split.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="split"></div>
+  <div class="split">
+    <div class="img-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -8,7 +10,17 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   .split
+    position: relative
     width: 100%
-    height: .2rem
+    height: 1rem
     background: #eee
+    .img-wrapper
+      position: absolute
+      top: .2rem
+      left: 50%
+      margin-left: -0.3rem
+      width: .6rem
+      height: .6rem
+      background: url("logo.png") no-repeat
+      background-size: 100% 100%
 </style>
